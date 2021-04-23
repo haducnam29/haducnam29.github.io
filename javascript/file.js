3af886ea8fdcfa41aa22eb98e4e8816e47dffb2a
haducnam29 <haducnam29@gmail.com>
2021-04-23 18:50:12 +0700

    java

---
+++ javascript/file.js
@@ -0,0 +1,24 @@
+// function bien(){
+// var i;
+// var tong= 0;
+// for(i=1;i<100;i++){
+//     tong+=i;
+    
+// }alert("tong la" + tong);}
+// bien();
+// function tonga_b(biena,bienb){
+// var tong =biena + bienb;
+// alert(tong);
+// }tonga_b(10,'20)
+// function mang(){
+//     var xemay=["honda","wade","laed"];
+// var show_xemay=xemay[1];
+// alert(show_xemay);
+// }
+// mang();
+function showMessage(){
+  alert("toi la ai") ; 
+}
+function showcontent(){
+    alert(document.getElementById("layid").value);
+}
